d032b34e6146a4412343fec256262e798111ce28
niquola <niquola@gmail.com>
2016-06-17 00:55:12 +0300

    end

--- src/clj/devconf/core.clj
+++ src/clj/devconf/core.clj
@@ -1,1 +1,82 @@
-(ns devconf.core)
+(ns devconf.core
+  (:require [org.httpkit.server :as http]
+            [hiccup.core :as html]
+            [ring.middleware.defaults :as mw]
+            [route-map.core :as rt]
+            [devconf.transport :as fmt]
+            [garden.core :as css]))
+
+(defn style []
+  (css/css
+   [:body {:padding "18px"}]))
+
+(defn js [src]
+  [:script {:src src :type "text/javascript"}])
+
+(defn layout [cnt]
+  (html/html
+   [:html
+    [:head
+     [:title "DevConff"]
+     #_[:style (style)]]
+    [:body cnt
+     (js "/js/app.js")]]))
+
+(defn index [req]
+  {:body (layout [:div#app
+                  [:p "Loading.."]])
+   :headers {"Content-Type" "text/html"}
+   :status 200})
+
+(defonce clinets (atom #{}))
+
+(defn broad-cast [msg]
+  (let [s (fmt/to-transit msg)]
+    (doseq [c @clinets]
+      (http/send! c s))))
+
+(defn do-eval [f]
+  (with-out-str
+    (println
+     (try (eval (read-string f))
+          (catch Exception e
+            (pr-str e))))))
+
+(do-eval "(println 1)")
+
+(defn on-message [raw-msg]
+  (let [msg (fmt/from-transit raw-msg)
+        msg (assoc msg :result "TODO")]
+    (println "message " msg)
+    (broad-cast msg)))
+
+(defn chan [req]
+  (http/with-channel req ch
+    (println "Incomming connection" ch)
+    (swap! clinets conj ch)
+    (http/on-receive ch #'on-message)
+    (http/on-close ch (fn [_] (swap! clinets disj ch)))))
+
+(def routes {:GET #'index
+             "chan" {:GET #'chan}})
+
+(defn dispatch [{uri :uri meth :request-method :as req}]
+  (if-let [r (rt/match [meth uri] routes)]
+    ((:match r) req)
+    {:body "Not found" :status 404 :headers {"Content-Type" "text"}}))
+
+
+(def app (-> dispatch
+             (mw/wrap-defaults mw/site-defaults)))
+
+(defonce stop (atom nil))
+
+(defn start []
+  (when-let [s @stop] (s))
+  (reset! stop
+          (http/run-server #'app {:port 8080})))
+
+(comment
+  (start)
+  (@stop)
+  )
--- src/cljs/devconf/core.cljs
+++ src/cljs/devconf/core.cljs
@@ -1,21 +1,70 @@
 (ns devconf.core
   (:require [reagent.core :as reagent :refer [atom]]
             [garden.core :as css]
+            [devconf.transport :as fmt]
             [devconf.transport :as tr]))
 
 
+(defonce state (atom {}))
+
+
+(defn chan-uri []
+  (str "ws://"
+       (.. js/window -location -host)
+       "/chan"))
+
+
+(defn send [raw-msg]
+  (let [msg (fmt/to-transit raw-msg)]
+    (when-let [s (:socket @state)]
+      (.send s msg))))
+
+
+(defn on-change [ev]
+  (swap! state assoc :input (.. ev -target -value)))
+
+(defn add-message [msg]
+  (swap! state update-in [:items] conj msg))
+
+(defn on-message [msg]
+  (let [msg (fmt/from-transit msg)]
+    (add-message msg)
+    (.log js/console "Message" msg)))
+
+(defn init-chan []
+  (when-let [s (:socket @state)] (.close s))
+  (let [s (js/WebSocket. (chan-uri))]
+    (.log js/console "Socket " s)
+    (aset s "onmessage" (fn [ev] (on-message (.-data ev))))
+    (swap! state assoc :socket s)))
+
+(defn on-submit [ev]
+  (when (and (= 13 (.-which ev))
+             (.-ctrlKey ev))
+    (send {:message (:input @state)
+                  :id (str (gensym))})
+    (aset (.-target ev) "value" "")))
+
 (defn $index []
   [:div#repl
-   [:h1 "Hello"]])
-
+   [:style (css/css [:textarea {:width "100%"
+                                :min-height "100px"}])]
+   [:textarea {:on-change on-change :on-key-down on-submit}]
+   (for [i (:items @state)]
+     [:div {:key (:id i)}
+      [:pre (:message i) " => " (:result i)]
+      [:hr]])])
 
 (defn mount-root []
   (reagent/render
    [$index] (.getElementById js/document "app")))
 
 (defn init! []
+  (init-chan)
   (mount-root))
 
 (comment
+
+  (send "Hello server")
 
   )
